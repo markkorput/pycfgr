96b39705612df6d389f0dfa3fe51f739d24a9caa
mark <mark.korput@gmail.com>
2019-09-21 10:03:48 +0200

    app.py and setup.pu

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cfgr',
+  version='0.0.1',
+  description='cfgr separates logic from data',
+  url='http://github.com/markkorput/pycfgr',
+  author='Short Notion',
+  author_email='shortnotion@gmail.com',
+  license='MIT',
+  packages=['cfgr'],
+  zip_safe=False)
