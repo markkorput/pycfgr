b622d812b3d226d50297c5477e6daa5309c7fb87
mark <mark.korput@gmail.com>
2019-09-27 14:42:52 +0200

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name='cfgr',
-  version='0.0.1',
+  version='0.0.2',
   description='cfgr separates logic from data',
   long_description=long_description,
   long_description_content_type='text/markdown',
