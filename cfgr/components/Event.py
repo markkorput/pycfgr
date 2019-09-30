f3459e1ba9984ff89df3330ca14480aa4c76a062
mark <mark.korput@gmail.com>
2019-09-30 15:18:50 +0200

    verbose option for Event component

--- cfgr/components/Event.py
+++ cfgr/components/Event.py
@@ -6,12 +6,20 @@ class Event:
   """
   @staticmethod
   def cfgr(builder):
+    builder.addInput('verbose').bool_to_method(lambda obj: obj.setVerbose)
     ## outputs
-    builder.addInput('on').to_method(lambda obj: obj.doEvent.fire)
+    builder.addInput('on').to_method(lambda obj: obj.fire)
     builder.addOutput('do').from_event(lambda obj: obj.doEvent)
 
   def __init__(self):
+    self.isVerbose = False
     self.doEvent = EventoEvent()
 
-  # def fire(self, *args, **kwargs):
-  #   self.doEvent(*args, **kwargs)
+  def fire(self, *args, **kwargs):
+    self.verbose('[Event] with {} args'.format(len(args)+len((kwargs))))
+    self.doEvent.fire(*args, **kwargs)
+
+  def setVerbose(self, v): self.isVerbose = v
+  def verbose(self, msg):
+    if self.isVerbose:
+      print(msg)
