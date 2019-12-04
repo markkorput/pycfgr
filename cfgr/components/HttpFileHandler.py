8bf98de3b016b2e96e08edd35dbced76caad222f
mark <mark.korput@gmail.com>
2019-12-04 13:02:48 +0100

    removed old code from HttpFileHandler

--- cfgr/components/HttpFileHandler.py
+++ cfgr/components/HttpFileHandler.py
@@ -90,44 +90,6 @@ class HttpFileHandler:
     self.verbose('[HttpFileHandler] wrote {} bytes to: {}'.format(file_length, filepath))
     self.savedEvent(filepath)
 
-  def processRequest_OLD(self, req):
-    """Saves a file following a HTTP PUT request"""
-    # https://f-o.org.uk/2017/receiving-files-over-http-with-python.html
-
-    # # Don't overwrite files
-    # if os.path.exists(filename):
-    #     self.send_response(409, 'Conflict')
-    #     self.end_headers()
-    #     reply_body = '"%s" already exists\n' % filename
-    #     self.wfile.write(reply_body.encode('utf-8'))
-    #     return
-
-    self.verbose('[HttpFileHandler] headers: {}'.format(req.handler.headers))
-
-    if not self.saveTo:
-      req.respond(501, 'Not Implemented')
-      return
-
-    filename = self.saveTo # if self.saveTo else os.path.basename(req.path)
-
-    # if self.keepExtension:
-    #   print("let's keep extension")
-    #   print(req.handler.headers)
-    #   print("request handler:")
-    #   print(req.handler)
-
-    file_length = int(req.handler.headers['Content-Length'])
-    with open(filename, 'wb') as output_file:
-      output_file.write(req.handler.rfile.read(file_length))
-    self.verbose('[HttpFileHandler] wrote {} bytes to: {}'.format(file_length, filename))
-
-    req.handler.send_response(201, 'Created')
-    req.handler.end_headers()    
-    reply_body = 'Saved "%s"\n' % filename
-    req.handler.wfile.write(reply_body.encode('utf-8'))
-
-    self.savedEvent(filename)
-
   def setVerbose(self, v):
     self.isVerbose = v
 
